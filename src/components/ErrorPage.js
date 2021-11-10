3839dc229dc348f1ad8e85f3afb3f3664000c298
Sebastian Alegrett Morreo <salegrett-morreo@huskers.unl.edu>
2021-11-10 02:55:35 -0600

    Error Page Formatting

--- src/components/ErrorPage.js
+++ src/components/ErrorPage.js
@@ -2,24 +2,29 @@ import React from 'react'
 import { useHistory } from 'react-router'
 import * as FaIcons from 'react-icons/fa'
 import { useTranslation } from 'react-i18next';
+import * as AiIcons from 'react-icons/ai';
+import './styles/ErrorPage.css'
 
 function ErrorPage(props) {
 
     const history = useHistory();
 
     const redirect = () => {
-        history.push(props.previous_page);
+        history.push(props.previousPage);
     }
 
     const { t } = useTranslation();
 
     return (
         <div className='error-page'>
-            <h1 className='error-page-title'>{t('error_prompt')}</h1>
-            <h2 className='error-page-subtitle'>{t('error_prompt_2')}</h2>
-            <h3 className='error-page-message'>{props.message}</h3>
-            <h2 className='error-page-subtitle'>{t('try_again')}</h2>
-            <button type='button'onClick={redirect}>{t('back')}<FaIcons.FaArrowCircleLeft/></button>
+            <div className='error-container'>
+                <AiIcons.AiFillBank className='error-icon' />
+                <h1 className='error-page-title'>{t('error_prompt')}</h1>
+                <h2 className='error-page-subtitle'>{t('error_prompt_2')}</h2>
+                <h3 className='error-page-message'>{props.message}</h3>
+                <h2 className='error-page-subtitle'>{t('try_again')}</h2>
+                <button className='error-button' type='button' onClick={redirect}>{t('back')} <FaIcons.FaArrowCircleLeft /></button>
+            </div>
         </div>
     )
 }
