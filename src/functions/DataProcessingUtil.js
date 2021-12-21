4060e7e57b5785871f1e2ec753c55109b9373ecc
Sebastian Alegrett Morreo <salegrett-morreo@huskers.unl.edu>
2021-12-21 12:04:07 -0600

    Data Processor updates

--- src/functions/DataProcessingUtil.js
+++ src/functions/DataProcessingUtil.js
@@ -20,9 +20,11 @@ export default class DataProcessingUtil {
             return obj.objects;
         })
 
-        /*ARRAY SETUP: elem[0] : ID, elem[1] : FICO No., elem[2] : Contract No., elem[3] : irrelevant
-                       elem[4] : Contract Name, elem[5] : Contract Start date, elem[6]: irrelevant, elem[7] : Trust type */
-        let trustInfo = trustArray.map((elem) => {
+        /*ARRAY SETUP: elem[0] : ID, elem[1] : Clx_Status (irrelevant to the customer), 
+                       elem[2] : Contract No., elem[3] : irrelevant, elem[4] : Contract Name, 
+                       elem[5] : Contract Start date, elem[6]: irrelevant, elem[7] : Trust type
+                       eem[9] : Company Code */
+        let trustInfo = trustArray.map((elem, index) => {
             let date = new Date(elem[5].contents); //Pretty format the date for usability.
             let year = new Intl.DateTimeFormat(Cookies.get('i18next'), { year: 'numeric' }).format(date);
             let month = new Intl.DateTimeFormat(Cookies.get('i18next'), { month: 'short' }).format(date);
@@ -30,58 +32,59 @@ export default class DataProcessingUtil {
             let dateString = `${year}-${month}-${day}`;
             let contractNo = this.removeUglyChars(elem[2].contents);
             let contractName = this.removeUglyChars(this.toTitleCase(elem[4].contents));
+            let companyCode = elem[9].contents.toUpperCase();
             let ficoType = this.removeUglyChars(this.toTitleCase(elem[7].contents));
             if (ficoType === "") {
                 ficoType = "Other";
             }
             return {
-                "trust": {
-                    headers: {  
-                        type: { label: 'Trust Type', value: ficoType },
-                        date: { label: 'Start Date', value: dateString },
-                        contract_name: { label: 'Contract Name', value: contractName }
-                    },
-                    body: [
-                        { label: 'Contract Number', value: contractNo },
-                        { label: 'Contract Name', value: contractName },
-                    ],
-                }
+                id: index,
+                headers: {
+                    type: ficoType,
+                    date: dateString,
+                    contract_name: contractName,
+                },
+                body: [
+                    { label: 'contract-no', value: contractNo },
+                    { label: 'company-code', value: companyCode }
+                ],
             }
         })
 
         let filteredInfo = trustInfo.filter((trust, index, self) => (
             index === self.findIndex((t) => (
-                trust.trust.headers.type.value !== "" &&
-                t.trust.headers.contract_name.value === trust.trust.headers.contract_name.value &&
-                t.trust.body[0].value === trust.trust.body[0].value
+                trust.headers.type !== "" &&
+                t.headers.contract_name === trust.headers.contract_name &&
+                t.body[0].value === trust.body[0].value
             ))
-        ))
+        ));
 
         let administration = filteredInfo.filter((t) => {
-            return t.trust.headers.type.value === 'Administration'
-        })
-
+            return (t.headers.type === 'Administration') ||
+                (t.headers.type === 'Administración');
+        });
         let other = filteredInfo.filter((t) => {
-            return t.trust.headers.type.value === 'Other'
-        })
-
+            return (t.headers.type === 'Other') ||
+                (t.headers.type === '') ||
+                (t.headers.type === 'Otro');
+        });
         let realEstate = filteredInfo.filter((t) => {
-            return t.trust.headers.type.value === 'Real State Developments'
-        })
-
+            return (t.headers.type === 'Real State Developments') ||
+                (t.headers.type === 'Desarrollos inmobiliarios');
+        });
         let investment = filteredInfo.filter((t) => {
-            return t.trust.headers.type.value === 'Investments'
-        })
-
-        console.log(filteredInfo);
-
+            return (t.headers.type === 'Investments') ||
+                (t.headers.type === 'inversiones');
+        });
         let guarantees = filteredInfo.filter((t) => {
-            return t.trust.headers.type.value === 'Guarantees'
+            return (t.headers.type === 'Guarantees') ||
+                (t.headers.type === 'Garantías');
         })
-
-        //other, administration
-
-        return [administration, other, realEstate, investment, guarantees];
+        let retirement = filteredInfo.filter((t) => {
+            return (t.headers.type === 'Retirement plan') ||
+                (t.headers.type === 'Previsión social')
+        });
+        return [administration, other, realEstate, investment, guarantees, retirement];
     }
 
     /**
@@ -100,51 +103,53 @@ export default class DataProcessingUtil {
             currency: 'USD',
         })
         /*
-        ARRAY SETUP: 0=ID, 1=IDCODE , 3=EFFTDATE, 4=STATUS  5=REVERSAL_PEND 6=TRANSTBL_CODE
+        ARRAY SETUP: 0=ID, 1=IDCODE, 2=IDNAME, 3=EFFTDATE, 4=STATUS  5=REVERSAL_PEND 6=TRANSTBL_CODE
         7=CURRENCY_CODE 8=TRANS_AMT 9=TAXABLE_AMT 10=TAX_AMT 11=TOTAL_AMT 12=EXCHANGE_RT 13=BASE_TRANS_AMT
         14=CONTRACT_NAME 15=DISBURSEMENT_TYPE 16=VOID 17=CHECK_NO 18=CHECK_PRINT_DATI 19=CHECK_PRINT_BY 
         */
-        let transactionInfo = transactionArray.map(elem => {
-            // let date = new Date(elem[3].contents); //Pretty format the date for usability.
-            // let year = new Intl.DateTimeFormat(Cookies.get('i18next'), { year: 'numeric' }).format(date);
-            // let month = new Intl.DateTimeFormat(Cookies.get('i18next'), { month: 'short' }).format(date);
-            // let day = new Intl.DateTimeFormat(Cookies.get('i18next'), { year: '2-digit' }).format(date);
-            // let dateString = `${year}-${month}-${day}`;
+        let transactionInfo = transactionArray.map((elem, index) => {
+            let date = new Date(elem[3].contents);
+            let year = new Intl.DateTimeFormat(Cookies.get('i18next'), { year: 'numeric' }).format(date);
+            let month = new Intl.DateTimeFormat(Cookies.get('i18next'), { month: 'short' }).format(date);
+            let day = new Intl.DateTimeFormat(Cookies.get('i18next'), { year: '2-digit' }).format(date);
+            let currency = (elem[7].contents.replace(/.*/, '')).toUpperCase();
+            let dateString = `${year}-${month}-${day}`;
             let status = this.removeUglyChars(this.toTitleCase(elem[4].contents));
             let amt = dollarUs.format(parseFloat(elem[11].contents));
             let contract = this.removeUglyChars(this.toTitleCase(elem[14].contents))
             let name = this.removeUglyChars(this.toTitleCase(elem[2].contents))
             return {
-                "transaction": {
-                    headers: {
-                        amount: { label: 'Ammount', value: amt },
-                        contract: { label: 'Transaction Name', value: name },
-                        status: { label: 'Status', value: status }
-                    },
-                    body: [
-                        { label: 'Contract Name', value: contract },
-                        { label: 'Transaction Name', value: name },
-                    ]
-                }
+                id: index,
+                headers: {
+                    amount: amt,
+                    name: name,
+                    date: dateString,
+                    status: status,
+                },
+                body: [
+                    { label: 'contract-name', value: contract },
+                    { label: 'currency', value: currency },
+                ]
             }
         })
         let filteredInfo = transactionInfo.filter((transaction, index, self) => (
             index === self.findIndex((t) => (
-                t.transaction.headers.contract.value === transaction.transaction.headers.contract.value &&
-                t.transaction.body[0].value === transaction.transaction.body[0].value
+                t.headers.name === transaction.headers.name &&
+                t.body[0].value === transaction.body[0].value
             ))
         ))
-
         let approved = filteredInfo.filter((transaction) => {
-            return transaction.transaction.headers.status.value === 'Approved';
+            return (transaction.headers.status === 'Approved') ||
+                (transaction.headers.status === 'Aprobada');
         })
         let pending = filteredInfo.filter((transaction) => {
-            return transaction.transaction.headers.status.value === 'Pending Approval';
+            return (transaction.headers.status === 'Pending Approval') ||
+                (transaction.headers.status === 'Pendiente de aprobar')
         })
         let rejected = filteredInfo.filter((transaction) => {
-            return transaction.transaction.headers.status.value === 'Rejected';
+            return (transaction.headers.status === 'Rejected') ||
+                (transaction.headers.status === 'Rechazada');
         })
-
         return [approved, pending, rejected];
     }
 
@@ -169,54 +174,59 @@ export default class DataProcessingUtil {
             let intPaid = dollarUs.format(parseFloat(elem[9].contents));
             let contract = this.removeUglyChars(this.toTitleCase(elem[5].contents));
             return {
-                "account": {
-                    headers: {
-                        account_no: { label: 'Account Number', value: accountNo },
-                        balance: { label: 'Balance', value: balance },
-                        linked_trust: { label: 'Linked Trust', value: contract },
-                        acc_type: { label: 'Account Type', value: accountType }
-                    },
-                    body: [
-                        { label: 'Company', value: company },
-                        { label: 'Interest Earned', value: intEarned },
-                        { label: 'Interest Paid', value: intPaid },
-                    ],
-                }
+                headers: {
+                    account_no: accountNo,
+                    balance: balance,
+                    linked_trust: contract,
+                    acc_type: accountType,
+                },
+                body: [
+                    { label: 'company', value: company },
+                    { label: 'int-earned', value: intEarned },
+                    { label: 'int-paid', value: intPaid },
+                ],
             }
         })
 
         let filteredInfo = accountInfo.filter((acc, index, self) => (
             index === self.findIndex((t) => (
-                t.account.headers.account_no.value === acc.account.headers.account_no.value
+                t.headers.account_no === acc.headers.account_no
             ))
         ))
 
         let realEstate = filteredInfo.filter((account) => {
-            return account.account.headers.acc_type.value === 'Real Estate'
+            return (account.headers.acc_type === 'Real Estate') ||
+                (account.headers.acc_type === 'Desarrollos inmobiliarios');
         });
 
         let checking = filteredInfo.filter((account) => {
-            return account.account.headers.acc_type.value === 'Checking Account'
+            return (account.headers.acc_type === 'Checking Account') ||
+                (account.headers.acc_type === 'Cuenta corriente');
         });
 
         let auto = filteredInfo.filter((account) => {
-            return account.account.headers.acc_type.value === 'Automobile'
+            return (account.headers.acc_type === 'Automobile') ||
+                (account.headers.acc_type === 'Automóbil')
         });
 
         let income = filteredInfo.filter((account) => {
-            return account.account.headers.acc_type.value === 'Fix Income'
+            return (account.headers.acc_type === 'Fix Income') ||
+                (account.headers.acc_type === 'Ingresos')
         });
 
         let loans = filteredInfo.filter((account) => {
-            return account.account.headers.acc_type.value === 'Loan'
+            return (account.headers.acc_type === 'Loan') ||
+                (account.headers.acc_type === 'Prestamo')
         });
 
         let other = filteredInfo.filter((account) => {
-            return account.account.headers.acc_type.value === 'Other'
+            return (account.account.headers.acc_type === 'Other') ||
+                (account.headers.acc_type === 'Otro') || (account.headers.acc_type === '')
         });
 
         let market = filteredInfo.filter((account) => {
-            return account.account.headers.acc_type.value === 'Equity Market, Stocks'
+            return (account.headers.acc_type === 'Equity Market, Stocks') ||
+                (account.headers.acc_type === 'Mercado')
         });
 
         return [realEstate, checking, auto, income, loans, other, market];
@@ -224,12 +234,12 @@ export default class DataProcessingUtil {
 
     findContract(contractName) {
         let contracts = JSON.parse(sessionStorage.getItem('myTrusts'));
-        return contracts.find(contract => contract.trust.headers.contract_name.value === contractName)
+        return contracts.find(contract => contract.headers.contract_name === contractName)
     }
 
     findAccount(contractName) {
         let accounts = JSON.parse(sessionStorage.getItem('myAccounts'));
-        return accounts.find(acc => acc.account.headers.linked_trust.value === contractName)
+        return accounts.find(acc => acc.headers.linked_trust === contractName)
     }
 
     /**
