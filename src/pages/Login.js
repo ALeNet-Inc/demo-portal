e37b424244caaa3495f6171d1e91c3fd4e4881c9
Sebastian Alegrett Morreo <salegrett-morreo@huskers.unl.edu>
2022-01-13 13:14:44 -0600

    Service request base64 file conversion complete

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import '../App.css';
-import Form2 from "../components/LoginForm"
+import LoginForm from "../components/LoginForm"
 
 /* Login Page for current users */
 
 function Login()  {
     return (
         <div className="login-background">
-            <Form2 />
+            <LoginForm />
         </div>
     )
 }
--- src/pages/ServiceRequests.js
+++ src/pages/ServiceRequests.js
@@ -40,6 +40,27 @@ function ServiceRequests() {
         { key: '405', value: t('serv405') }
     ]
 
+    const convertBase64 = (file) => {
+        return new Promise((resolve, reject) => {
+          const fileReader = new FileReader();
+          fileReader.readAsDataURL(file);
+    
+          fileReader.onload = () => {
+            resolve(fileReader.result);
+          };
+    
+          fileReader.onerror = (error) => {
+            reject(error);
+          };
+        });
+      };
+
+      const uploadImage = async (e) => {
+        const file = e.target.files[0];
+        const base64 = await convertBase64(file);
+        console.log(base64);
+    };
+
     return (
         <div className='service-requests'>
             <Sidebar />
@@ -50,7 +71,7 @@ function ServiceRequests() {
                     <Dropdown id='service-dropdown' placeholder={t('what-help-with')} options={serviceRequestOptions} onChange={dropdownHandler} />
                     <br />
                     <label className='upload-prompt'>
-                        <input type='file' className='fileUpload' />
+                        <input type='file' className='fileUpload' onChange={ (e) => uploadImage(e) }/>
                         Upload File
                     </label>
                 </div>
